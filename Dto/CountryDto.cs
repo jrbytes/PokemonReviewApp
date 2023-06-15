c976c56f30388539205ab09dd31c35354fd22dea
Jr Bytes <itamazor@gmail.com>
2023-06-15 12:36:18 -0300

    add country module

---
+++ Dto/CountryDto.cs
@@ -0,0 +1,11 @@
+// using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Dto
+{
+  public class CountryDto
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    // public ICollection<Owner> Owners { get; set; }
+  }
+}
