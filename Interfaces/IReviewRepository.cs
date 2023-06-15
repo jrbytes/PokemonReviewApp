3fba20072d012418cb9c38779ab46fb687d9dfa0
Jr Bytes <itamazor@gmail.com>
2023-06-15 15:02:13 -0300

    add reviee module

---
+++ Interfaces/IReviewRepository.cs
@@ -0,0 +1,12 @@
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+  public interface IReviewRepository
+  {
+    ICollection<Review> GetReviews();
+    Review GetReview(int reviewId);
+    ICollection<Review> GetReviewsOfAPokemon(int pokeId);
+    bool ReviewExists(int reviewId);
+  }
+}
