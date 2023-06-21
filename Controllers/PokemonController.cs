914148e7462a265b2398c26182bfc8b3a2101f63
Jr Bytes <itamazor@gmail.com>
2023-06-21 13:10:32 -0300

    add update to pokemon module

--- Controllers/PokemonController.cs
+++ Controllers/PokemonController.cs
@@ -11,11 +11,15 @@ namespace PokemonReviewApp.Controllers
   public class PokemonController : Controller
   {
     private readonly IPokemonRepository _pokemonRepository;
+    private readonly IReviewRepository _reviewRepository;
     private readonly IMapper _mapper;
 
-    public PokemonController(IPokemonRepository pokemonRepository, IMapper mapper)
+    public PokemonController(IPokemonRepository pokemonRepository,
+      IReviewRepository reviewRepository,
+      IMapper mapper)
     {
       _pokemonRepository = pokemonRepository;
+      _reviewRepository = reviewRepository;
       _mapper = mapper;
     }
 
@@ -93,6 +97,35 @@ namespace PokemonReviewApp.Controllers
       }
 
       return Ok("Successfully created");
+    }
+
+    [HttpPut("{pokeId}")]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(404)]
+    public IActionResult UpdatePokemon(int pokeId, PokemonDto updatedPokemon)
+    {
+      if(updatedPokemon == null)
+        return BadRequest(ModelState);
+
+      if(pokeId != updatedPokemon.Id)
+        return BadRequest(ModelState);
+
+      if(!_pokemonRepository.PokemonExists(pokeId))
+        return NotFound();
+
+      if(!ModelState.IsValid)
+        return BadRequest();
+
+      var pokemonMap = _mapper.Map<Pokemon>(updatedPokemon);
+
+      if(!_pokemonRepository.UpdatePokemon(pokemonMap))
+      {
+        ModelState.AddModelError("", "Something went wrong updating category");
+        return StatusCode(500, ModelState);
+      }
+
+      return NoContent();
     }
   }
 }
