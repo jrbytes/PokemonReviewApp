11b21ab61ba35f4cc828678ef8648fff2f746941
Jr Bytes <itamazor@gmail.com>
2023-06-22 12:53:26 -0300

    add delete to category module

--- Repository/CategoryRepository.cs
+++ Repository/CategoryRepository.cs
@@ -50,5 +50,11 @@ namespace PokemonReviewApp.Repository
       var saved = _context.SaveChanges();
       return saved > 0 ? true : false;
     }
+
+    public bool DeleteCategory(Category category)
+    {
+      _context.Remove(category);
+      return Save();
+    }
   }
 }
